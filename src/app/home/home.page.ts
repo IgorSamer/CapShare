8a2536640506f553456acfecf65077726a1d52cf
Igor Remas <igoremascordeiro@hotmail.com>
2023-01-03 20:30:59 -0300

    initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,44 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public image: string = 'https://ionicframework.com/docs/icons/feature-component-tabs-icon.png';
 
-  constructor() {}
+  constructor() { }
 
+  async saveImage() {
+    try {
+      const response: Response = await fetch(this.image);
+      const blob: Blob = await response.blob();
+      const reader = new FileReader();
+
+      reader.onloadend = async () => {
+        const file = await Filesystem.writeFile({
+          data: reader.result as string,
+          directory: Directory.Cache,
+          path: `${Date.now()}.png`
+        });
+
+        console.log(file.uri);
+
+        this.shareImage(file.uri);
+      };
+
+      reader.readAsDataURL(blob);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async shareImage(fileUri: string) {
+    try {
+      const shareResult = await Share.share({
+        dialogTitle: 'Share image',
+        url: fileUri
+      });
+
+      console.log(shareResult.activityType);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
